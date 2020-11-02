a19f2adf7daef1013523ed6282356f1270266574
jf uccelli <jf.uccelli@telaqua.com>
2020-11-02 15:28:06 +0100

    add url to icon weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-template-curly-in-string */
 import React, { useState } from 'react';
 const api = {
-  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
+  key: "6654f8cea88d659f746234255cd4fb63",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
@@ -10,16 +11,16 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter") {
+      // fetch(`${api.base}forecast?q=${query}&units=metric&cnt=3&APPID=${api.key}`)
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
           setQuery('');
-          console.log(result);
         });
     }
+     
   }
-
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -31,13 +32,15 @@ function App() {
 
     return `${day} ${date} ${month} ${year}`
   }
-
+  
+  
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+   
+    <div className={(typeof weather.main !== "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
-            type="text"
+            type="search"
             className="search-bar"
             placeholder="Search..."
             onChange={e => setQuery(e.target.value)}
@@ -46,17 +49,15 @@ function App() {
           />
         </div>
         {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
+        <div className="container">
             <div className="location">{weather.name}, {weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
             <div className="temp">
               {Math.round(weather.main.temp)}°c
             </div>
             <div className="weather">{weather.weather[0].main}</div>
-          </div>
+            <div className="wind">Win speed: {Math.round(weather.wind.speed*3.6)} Km/h</div>
+            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='' />
         </div>
         ) : ('')}
       </main>
